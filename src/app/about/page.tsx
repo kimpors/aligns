c2582d1c52bb9763a3027b579a268826a5cef229
kimpors <mikita_korshak@ukr.net>
2023-12-17 18:19:50 +0200

    Fill all with meaningful text
    a little improve UI

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,9 +12,44 @@ const About = () =>
         About us
       </Header>
       <section className={ styles.about }>
-        <h2>Why choose us</h2>
+      <h2>About us</h2>
         <p>
-          Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat. 
+          Welcome to WebHost - your go-to destination for reliable
+          web hosting. We're dedicated to fueling your online
+          success with cutting-edge solutions and unwavering support.
+        </p>
+
+        <h2>Why WebHost?</h2>
+        <section>
+          <h3>Expert Team</h3>
+          <p>
+            Trust our seasoned professionals to deliver
+            expertise that keeps your website ahead in
+            the digital game.
+          </p>
+
+          <h3>Customer Focused</h3>
+          <p>
+            Experience 24/7 support focused on your satisfaction.
+            We're here to troubleshoot, answer questions,
+            and ensure your hosting is seamless.
+          </p>
+
+          <h3>Leading Technology</h3>
+          <p>
+            Embrace the future with our state-of-the-art
+            infrastructure, providing fast, secure, and scalable
+            hosting solutions.
+          </p>
+        </section>
+
+
+        <h2>Join us</h2>
+        <p>
+          Whether you're a blogger or a business owner, make
+          WebHost your partner in online success. We're here
+          to turn your digital ambitions into reality.
+          Thanks for choosing us – let's build success together!
         </p>
       </section>
       <Footer />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,13 @@ import Footer from './components/Footer/Footer';
 
 import styles from './Home.module.css';
 
-
 const links = [ "/plans", "/login", "/about" ];
-const description = "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.";
+
+const basic = "Our Basic Plan is perfect for individuals and small businesses looking to establish their online presence. With reliable hosting services, you get a solid foundation for your website without breaking the bank. Enjoy ample storage space, essential features, and responsive customer support to ensure your website is up and running smoothly.";
+
+const standart = "Upgrade to our Standard Plan for a more robust hosting experience. Ideal for growing businesses and websites with moderate traffic, this plan offers increased resources, faster loading times, and enhanced security features. Benefit from advanced tools and a user-friendly control panel to manage your site effortlessly. Our Standard Plan strikes the right balance between performance and affordability."
+
+const advanced = "For high-performance websites and demanding online ventures, our Advanced Plan is the ultimate solution. Unleash the full potential of your website with top-tier hosting infrastructure, priority support, and advanced security measures. Enjoy lightning-fast load times, generous resource allocations, and the flexibility to customize your hosting environment. The Advanced Plan is designed for businesses that prioritize speed, reliability, and scalability for their online success."
 
 function Home()
 {
@@ -17,20 +21,32 @@ function Home()
       <Header links={ links }>
         Web Hosting
       </Header>
-      <Card className={ styles.item1} title="Basic" description={ description } link="/plans" />
-      <Card className={ styles.item2 } title="Standart" description={ description } link="/plans" />
-      <Card className={ styles.item3 } title="Advanced" description={ description } link="/plans" />
+      <Card className={ styles.item1} title="Basic" description={ basic } link="/plans" />
+      <Card className={ styles.item2 } title="Standart" description={ standart } link="/plans" />
+      <Card className={ styles.item3 } title="Advanced" description={ advanced } link="/plans" />
       <article className={ styles.info }>
         <h2>Perfect solution</h2>
         <section>
           <p>
-            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+            <h3>Unbeatable Reliability</h3>
+            Count on us for uninterrupted website performance,
+            minimal downtime, and swift loading times. Our robust
+            infrastructure ensures a consistently reliable hosting
+            experience.
           </p>
+
           <p>
-            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+            <h3>Seamless Scalability</h3>
+            Adapt effortlessly to your growing needs. Upgrade your
+            hosting plan with ease as your website expands, accommodating
+            increased traffic and resource demands.
           </p>
+
           <p>
-            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+            <h3>Support and Security Excellence</h3>
+            Experience dedicated customer support 24/7. Our advanced
+            security features shield your website from threats,
+            providing a worry-free environment for your online presence.
           </p>
         </section>
       </article>
