fbcb89d168408a3bea6dee74d517045bc0ad881d
kimpors <mikita_korshak@ukr.net>
2024-01-16 11:42:43 +0200

    Fix footer render

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,19 +1,22 @@
-import styles from './Footer.module.css';
+import styles from './styles.module.css';
 
 const Footer = () => 
 {
   return (
     <footer className={ styles.footer }>
-      <h2>Web Host</h2>
 
-      <section>
-        <p>
-          copyright@something
-        </p>
-        <p>
-          contacts
-        </p>
-      </section>
+      <article>
+        <h2>Web Host</h2>
+
+        <section>
+          <p>
+            copyright@something
+          </p>
+          <p>
+            contacts
+          </p>
+        </section>
+      </article>
     </footer>
   )
 }
